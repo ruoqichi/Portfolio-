d653aab3d301d9f2cb29e2d3497dc36a7f79a75e
Anna Chi <annachi@live.cn>
2019-09-03 23:16:10 -0700

    1

--- js/script.js
+++ js/script.js
@@ -2,31 +2,16 @@
 const sideNav = document.querySelector('.sidenav');
 M.Sidenav.init(sideNav, {});
 
-var slideIndex = 1;
-showSlides(slideIndex);
+$(document).ready(function () {
+  $('.materialboxed').materialbox()
+})
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+$(document).ready(function () {
+  $('.carousel').carousel({
+    fullWidth: false,
+    dist: 0,
+    indicators: true,
+    duration: 100
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+  )
+})
